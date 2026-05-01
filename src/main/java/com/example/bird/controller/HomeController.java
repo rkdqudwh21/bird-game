8f35620249621593b96d7eb236339cbdcb4307cb
rkdqudwh21 <rkdqudwh21@naver.com>
2026-05-01 16:18:48 +0900

    feat: 새 먹이주기와 성장 단계 구현

--- src/main/java/com/example/bird/controller/HomeController.java
+++ src/main/java/com/example/bird/controller/HomeController.java
@@ -3,10 +3,38 @@ package com.example.bird.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
 
 
 @Controller
 public class HomeController {
+
+private int affection = 0;
+private String getBirdStage(){
+    if (affection>=20){
+        return "닭";
+    }
+    if (affection>=10){
+        return "병아리";
+    }
+
+    return "알";
+}
+
+private String getBirdName(){
+    if (affection>=20){
+        return "첫번째 닭";
+    }
+    if (affection>=10){
+        return "첫 번째 병아리";
+    }
+     return "첫 번째 알";
+}
 
     @GetMapping("/")
     public String home(){
@@ -15,9 +43,28 @@ public class HomeController {
     
     @GetMapping("/birds")
     public String birds(Model model){
-        model.addAttribute("birdName", "첫번째 알");
-        model.addAttribute("birdStage", "알");
+        model.addAttribute("birdName", getBirdName());
+        model.addAttribute("birdStage", getBirdStage());
         model.addAttribute("affection", 0);
         return "birds";
     } 
+
+    @GetMapping("/birds/{id}")
+    public String birdDetail(@PathVariable int id,Model model) {
+        model.addAttribute("birdName", getBirdName());
+        model.addAttribute("birdStage", getBirdStage());
+        model.addAttribute("affection", affection);
+        model.addAttribute("birdId", id);
+        return "bird-detail";
+    }
+
+    @PostMapping("/birds/{id}/feed")
+    public String feed(@PathVariable int id, RedirectAttributes redirectAttributes){
+        affection +=1;
+        
+        redirectAttributes.addFlashAttribute("messages","먹이를 줬습니다.(+1)");
+        return "redirect:/birds/" + id;
+    }
 }
+    
+    
